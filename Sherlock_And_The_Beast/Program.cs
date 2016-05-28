f96f2e5cb15d86684d8bfc9fd2599d2fef6a84ed
JeffreyMJohnson <jefjohms@gmail.com>
2016-05-27 17:57:36 -0700

    eod

--- Sherlock_And_The_Beast/Program.cs
+++ Sherlock_And_The_Beast/Program.cs
@@ -32,126 +32,88 @@ Print the largest Decent Number having N digits; if no such number exists, tell 
  */
 namespace Sherlock_And_The_Beast
 {
-    class Program
+    public class Program
     {
-        private static string[] _sampleInput =
-        {
-            "4",
-            "1",
-            "3",
-            "5",
-            "11"
-        };
-
-        static bool IsDecentNumber(Int64 testNum)
+        
+        public static bool IsDecentNumber(string testNum)
         {
             /*Its digits can only be 3's and/or 5's.
 The number of 3's it contains is divisible by 5.
 The number of 5's it contains is divisible by 3.
 If there are more than one such number, we pick the largest one.*/
-            string testNumString = testNum.ToString();
-            int fiveCount = testNumString.Count(c => c == '5');
-            int threeCount = testNumString.Count(c => c == '3');
-            bool isOnlyFiveAndThree = fiveCount + threeCount == testNumString.Length;
+            int fiveCount = testNum.Count(c => c == '5');
+            int threeCount = testNum.Count(c => c == '3');
+            bool isOnlyFiveAndThree = fiveCount + threeCount == testNum.Length;
             bool numFivesDivisibleByThree = fiveCount % 3 == 0;
             bool numThreesDivisibleByFive = threeCount % 5 == 0;
             return isOnlyFiveAndThree && numThreesDivisibleByFive && numFivesDivisibleByThree;
         }
 
-        static string IncrementNum(string numAsString)
+        public static string LargestDecentNumber(int numDigits)
         {
-            /*
-                * Start at the LSB (least significant bit, i.e. right most bit); 
-                * while the current bit is 1, change it to 0 and move left, stopping when we reach far left or hit a 0 bit, which we change to 1 and stop
-                */
-            string numString = numAsString;
-            for (int i = numString.Length - 1; i >= 0; i--)
+            
+            int tailNum = numDigits % 15;
+            string tail = ""; //hotdog
+            switch (tailNum)
             {
-                if (numString[i] == '3')
-                {
-                    char[] c = numString.ToCharArray();
-                    c[i] = '5';
-                    numString = new string(c);
+                case 0:
+                case 1:
+                case 2:
+                    goto case 13;
                     break;
-                }
-                if (i > 0)
-                {
-                    char[] c = numString.ToCharArray();
-                    c[i] = '3';
-                    numString = new string(c);
-                }
-
+                case 3:
+                    tail = "555";
+                    break;
+                case 4:
+                    goto case 13;
+                    break;
+                case 5:
+                    tail = "33333";
+                    break;
+                case 6:
+                    tail = "555555";
+                    break;
+                case 7:
+                    goto case 13;
+                    break;
+                case 8:
+                    tail = "55533333";
+                    break;
+                case 9:
+                    tail = "555555555";
+                    break;
+                case 10:
+                    tail = "3333333333";
+                    break;
+                case 11:
+                    tail = "55555533333";
+                    break;
+                case 12:
+                    tail = "555555555555";
+                    break;
+                case 13:
+                    tail = "-1";
+                    break;
+                case 14:
+                    tail = "555555555333333";
+                    break;
             }
-            return numString;
-        }
 
-        static string FilledString(char fillChar, Int64 numDigits)
-        {
             string result = "";
-            for (Int64 i = 0; i < numDigits; i++)
+            for (int i = 0; i < numDigits - tailNum; i++)
             {
-                result += fillChar;
+                result += '5';
             }
-            return result;
-        }
+            result += tail;
 
-        static Int64 LargestDecentNumber(Int64 numDigits)
-        {
-            /*
-             * create string of '3' length of given numDigits
-             * 
-             * while string num is not string of all '5'
-             *  if string num is a decent number 
-             *      add to list
-             *  increment string num
-             * if string num is a decent number
-             *  add to list
-             *  
-             *  
-             */
-
-
-            string numString = FilledString('3', numDigits);
-            string testString = FilledString('5', numDigits);
-            List<Int64> decentNumbers = new List<Int64>();
-            do
+            if (IsDecentNumber(result))
             {
-                Int64 num = Int64.Parse(numString);
-                if (IsDecentNumber(num))
-                {
-                    decentNumbers.Add(num);
-                }
-                numString = IncrementNum(numString);
-
-            } while (numString != testString);
-           
-
-
-            if (IsDecentNumber(Int64.Parse(numString)))
-            {
-                decentNumbers.Add(Int64.Parse(numString));
-            }
-            if (decentNumbers.Count > 1)
-            {
-                Int64 num = 0;
-                foreach (Int64 decentNumber in decentNumbers)
-                {
-                    num = Math.Max(num, decentNumber);
-                }
-                return num;
-            }
-            else if (decentNumbers.Count > 0)
-            {
-                return decentNumbers[0];
+                return result;
             }
             else
             {
-                return -1;
+                return "-1";
             }
-
-
-
-
         }
 
 
@@ -165,7 +127,7 @@ The T subsequent lines each contain an integer, N, detailing the number of digit
             int numTests = Int32.Parse(Console.ReadLine());
             for (int i = 0; i < numTests; i++)
             {
-                Int64 testNum = Int64.Parse(Console.ReadLine());
+                int testNum = int.Parse(Console.ReadLine());
                 Console.WriteLine(LargestDecentNumber(testNum));
             }
 
